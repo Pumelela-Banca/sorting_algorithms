18451f7b8d76ce07183d6229a8f3df78b2bdaa42
Pumelela-Banca <pumelelabanca@gmail.com>
2023-07-25 22:59:22 +0200

    added quick sort prototypes

--- sort.h
+++ sort.h
@@ -25,6 +25,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void swap_nodes(listint_t **list, listint_t *curr);
-
-
+size_t partition(int *array, size_t low, size_t high, size_t size);
+void r_sort(int *array, size_t low, size_t high, size_t size);
+void quick_sort(int *array, size_t size);
 #endif
