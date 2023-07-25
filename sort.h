7104f0409585a1b722fe33ae32c73ad1a17690eb
Pumelela-Banca <pumelelabanca@gmail.com>
2023-07-25 13:14:40 +0200

    added prototype for swap nodes

--- sort.h
+++ sort.h
@@ -24,6 +24,7 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void swap_nodes(listint_t **list, listint_t *curr);
 
 
 #endif
